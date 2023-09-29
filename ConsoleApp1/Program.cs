c1009466f099b58a55afbc11bc27fb93643a0cf8
Administrator <Administrator@HPLAPTOP>
2023-09-29 01:54:29 -0600

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+// Define the IQuittable interface with a Quit() method
+interface IQuittable
+{
+    void Quit();
+}
+
+// Create an Employee class that inherits from IQuittable
+class Employee : IQuittable
+{
+    // Implement the Quit() method
+    public void Quit()
+    {
+        Console.WriteLine("Employee has quit their job.");
+        // You can add more logic here if needed
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create an instance of Employee
+        Employee employee = new Employee();
+
+        // Use polymorphism to create an object of type IQuittable
+        IQuittable quittableEmployee = employee;
+
+        // Call the Quit() method on the IQuittable object
+        quittableEmployee.Quit();
+
+        // Output:
+        // Employee has quit their job.
+    }
+}
